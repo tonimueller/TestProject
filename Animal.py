e106de13b01c84bedfa6cfdfac4bdf3d60c65618
ToniM <toni.mueller@posteo.de>
2022-11-14 14:55:34 +0100

    Added more features and their output to the Animal class.

--- Animal.py
+++ Animal.py
@@ -5,9 +5,22 @@ class Animals:
     def __init__(self, name):
         self.name = name
         self.size = 0
+        self.age = 0
 
     def print_name(self):
         print(self.name)
 
-    def add_size(self, size ):
+    def print_size(self):
+        print(f'The animal, {self.name}, is of size {self.size}cm.')
+
+    def print_age(self):
+        print(f'The animal, {self.name}, is {self.age} years old.')
+
+    def set_size(self, size ):
         self.size = size
+
+    def set_age(self, age):
+        self.age = age
+
+    def increment_age(self):
+        self.age += 1
--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from Animal import Animals
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    frog = Animals('froggy')
+    print(f'Get a new pet: ')
+    frog = Animals('Froggy')
     frog.print_name()
+    frog.set_size(10)
+    frog.set_age(2)
+    frog.print_size()
+    frog.print_age()
+    print (f'One year later...')
+    frog.increment_age()
+    frog.print_age()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
