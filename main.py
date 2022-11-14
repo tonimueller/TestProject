0714ce569ba1b3b510ad1b8bee994099989872f1
ToniM <toni.mueller@posteo.de>
2022-11-14 14:42:50 +0100

    Erase Fibonacci calculation and start adding an animal mother class.

--- main.py
+++ main.py
@@ -1,44 +1,9 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-def print_fibonacci(nterms):
-    # first two terms
-    n1, n2 = 0, 1
-    count = 0
-
-    # check if the number of terms is valid
-    if nterms <= 0:
-        print("Please enter a positive integer")
-    # if there is only one term, return n1
-    elif nterms == 1:
-        print("Fibonacci sequence upto", nterms, ":")
-        print(n1)
-    # generate fibonacci sequence
-    else:
-        print("Fibonacci sequence:")
-        while count < nterms:
-            print(n1)
-            nth = n1 + n2
-            # update values
-            n1 = n2
-            n2 = nth
-            count += 1
-
+from Animal import Animals
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    #   values = ('l', [0,1,1,2,3,5,8,12])
-    nTerms = 5
-    print(f'Print the Fibonacci series for the first {nTerms}')
-    print_fibonacci(nTerms)
+    frog = Animals('froggy')
+    frog.print_name()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
