5d178f12f763bcb917bdc114af17ad592cfd7efd
ToniM <toni.mueller@posteo.de>
2022-11-14 14:27:30 +0100

    Add the calculation of the Fibonacci numbers.

--- main.py
+++ main.py
@@ -9,8 +9,36 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
+def print_fibonacci(nterms):
+    # first two terms
+    n1, n2 = 0, 1
+    count = 0
+
+    # check if the number of terms is valid
+    if nterms <= 0:
+        print("Please enter a positive integer")
+    # if there is only one term, return n1
+    elif nterms == 1:
+        print("Fibonacci sequence upto", nterms, ":")
+        print(n1)
+    # generate fibonacci sequence
+    else:
+        print("Fibonacci sequence:")
+        while count < nterms:
+            print(n1)
+            nth = n1 + n2
+            # update values
+            n1 = n2
+            n2 = nth
+            count += 1
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    #   values = ('l', [0,1,1,2,3,5,8,12])
+    nTerms = 5
+    print(f'Print the Fibonacci series for the first {nTerms}')
+    print_fibonacci(nTerms)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
